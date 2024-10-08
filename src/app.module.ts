d06bcd6536a9ff31486f84bf2618345d3d956c58
belenespilman <belen.espilman@gmail.com>
2024-10-08 15:55:10 -0300

    Generate services for 6 entities

--- src/app.module.ts
+++ src/app.module.ts
@@ -7,10 +7,24 @@ import { CompradoresController } from './controllers/compradores.controller';
 import { CategoriasController } from './controllers/categorias.controller';
 import { PedidosController } from './controllers/pedidos.controller';
 import { OperadoresController } from './controllers/operadores.controller';
+import { ProductosService } from './services/productos.service';
+import { CategoriasService } from './services/categorias.service';
+import { PedidosService } from './services/pedidos.service';
+import { CompradoresService } from './services/compradores.service';
+import { FabricantesService } from './services/fabricantes.service';
+import { OperadoresService } from './services/operadores.service';
 
 @Module({
   imports: [],
-  controllers: [AppController, FabricantesController, ProductosController, CompradoresController, CategoriasController, PedidosController, OperadoresController],
-  providers: [AppService],
+  controllers: [
+    AppController,
+    FabricantesController,
+    ProductosController,
+    CompradoresController,
+    CategoriasController,
+    PedidosController,
+    OperadoresController,
+  ],
+  providers: [AppService, ProductosService, CategoriasService, PedidosService, CompradoresService, FabricantesService, OperadoresService],
 })
 export class AppModule {}
