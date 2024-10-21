6e589d1025d9116b58bc01b8c50f3da79165bd87
belenespilman <belen.espilman@gmail.com>
2024-10-21 16:51:11 -0300

    Add documentation with Swagger

--- src/operadores/controllers/compradores.controller.ts
+++ src/operadores/controllers/compradores.controller.ts
@@ -1,4 +1,6 @@
 import { Controller } from '@nestjs/common';
+import { ApiTags, ApiOperation } from '@nestjs/swagger';
 
+@ApiTags('Compradores')
 @Controller('compradores')
 export class CompradoresController {}
