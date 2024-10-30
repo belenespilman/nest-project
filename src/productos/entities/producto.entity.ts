83126a3a9783fcfeb175872e058d1d2b3e97c7ad
belenespilman <belen.espilman@gmail.com>
2024-10-30 00:24:16 -0300

    Transformar entidades al tipo de TypeOrm

--- src/productos/entities/producto.entity.ts
+++ src/productos/entities/producto.entity.ts
@@ -1,9 +1,28 @@
+import { PrimaryGeneratedColumn, Column, Entity } from 'typeorm';
+
+@Entity()
 export class Producto {
+  @PrimaryGeneratedColumn()
   id: number;
+
+  @Column({ type: 'varchar', length: 255, unique: true })
   nombre: string;
+
+  @Column({ type: 'text' })
   descripcion: string;
+
+  @Column({ type: 'int' })
   precio: number;
+
+  @Column({ type: 'int' })
   stock: number;
+
+  @Column({ type: 'varchar' })
   origen: string;
+
+  @Column({ type: 'varchar' })
   imagen: string;
+
+  // @ManyToMany(() => Pedido, pedido => pedido.productos)
+  // pedidos: Pedido[];
 }
